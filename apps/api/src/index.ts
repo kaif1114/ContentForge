c46bd5bb9bc16e08979077724a193c82e5f3da36
kaifm134 <kaif.m134@gmail.com>
2025-03-06 21:31:12 +0500

    api: initial mongoDB setup

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,13 @@
 import express from "express"
 import repurpose from "./routes/repurpose"
+import mongoose from "mongoose"
+import 'dotenv/config'
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
+  console.log("Connected to MongoDB")
+}).catch((err) => {
+  console.log("Error connecting to MongoDB", err)
+})
 
 const app = express()
 
@@ -11,6 +19,8 @@ app.use("/scrape", repurpose)
 app.get("/", (req, res) => {
   res.send("Server is running")
 })
+
+
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001")
