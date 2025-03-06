66d5210912b642d629f94b82412a7ee09412320a
kaifm134 <kaif.m134@gmail.com>
2025-03-06 22:14:02 +0500

    api: user and content schema added

---
+++ apps/api/src/models/Content.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const contentSchema = new mongoose.Schema({
+    url: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    scrapedContent: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+})
+
+const Content = mongoose.model("Content", contentSchema)
+
+export default Content
