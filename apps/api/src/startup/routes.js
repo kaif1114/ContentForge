4394a2335fddd323332efaa94b36622fdbb721cb
kaifm134 <kaif.m134@gmail.com>
2025-05-11 17:22:20 +0500

    api: names for files containing router changed to index for clarity

--- apps/api/src/startup/routes.js
+++ apps/api/src/startup/routes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import postsRouter from "../routes/posts/posts.js";
-import ideasRouter from "../routes/ideas/ideas.js";
+import postsRouter from "../routes/posts/index.js";
+import ideasRouter from "../routes/ideas/index.js";
 import scrape from "../routes/scrape.js";
 import content from "../routes/content-sources.js";
-import auth from "../routes/auth/auth.js";
+import auth from "../routes/auth/index.js";
 import authMiddleware from "../middleware/auth.js";
 import { errorHandler } from "../middleware/error.js";
-import schedule from "../routes/schedule/schedule.js";
+import schedule from "../routes/schedule/index.js";
 export function routes(app) {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
