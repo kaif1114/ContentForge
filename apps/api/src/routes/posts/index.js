c22fae8c71db174a6a9b57cee89b916d81f97e7e
Tanveer Hamza <152054823+TanveerHamza@users.noreply.github.com>
2025-06-17 11:02:59 +0500

    changess and error resolved

--- apps/api/src/routes/posts/index.js
+++ apps/api/src/routes/posts/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import getPosts from "./get-posts.js";
+import getSinglePost from "./get-single-post.js";
 import generate from "./generate.js";
 import generateFromIdea from "./generate-from-idea.js";
 import deletePost from "./delete.js";
@@ -8,6 +9,7 @@ import modify from "./modify.js";
 const router = express.Router();
 
 router.get("/", getPosts);
+router.get("/:postId", getSinglePost);
 router.post("/generate", generate);
 router.post("/generate-from-idea", generateFromIdea);
 router.delete("/:postId", deletePost);
